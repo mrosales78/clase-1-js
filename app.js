ffacc8d9abb987809d14a5900a986b782ecbd36b
mrosales78 <mrosales78@gminit>
2022-12-21 00:38:08 -0300

    javascript

---
+++ app.js
@@ -0,0 +1,37 @@
+//comentario de una sola linea
+/* comentario de
+mas de una  linea*/
+
+
+// declaracion de variables
+//tipos de datos que existen
+
+/*  numericos entre 1...10000, reales etc
+    cadena de caracteres(strings)
+    valores logicos(true y false o 1 y 0)
+*/
+
+/*
+    var numero = 96
+
+    let palabras = "pedro"
+
+    const numeroDepi = 3.1416
+
+    let esnuevo = true
+
+    let numero = 101
+*/
+
+/*
+    let numero = 63
+    console.log("el valor inicial de numero es" , numero)
+    let nuevonumero = 9875465
+    console.log("el valor iniciar de nuevonumero es", nuevonumero)
+
+    nuevonumero = numero
+    console.log ("este es el valor de la ultima variable" ,nuevonumero)
+*/
+
+
+
