109a66bf372249b0b0a5e860f1a5dbb2bddb8c58
mrosales78 <mrosales78@gminit>
2022-12-28 21:23:05 -0300

    clase 4

---
+++ clase4/app.js
@@ -0,0 +1,28 @@
+
+//funciones
+//3 partes
+
+//1 parte declaracion y la llamada de la funcion
+
+
+/* function mostrarnombre(hoyesmiercoles){
+    console.log(`eso es el console.log de la funcion hoyesmiercoles`)
+    return hoyesmiercoles
+} */
+
+
+function multiplique (a,b){
+    let resultadomulti= (3*5)
+    //console.log(`eso es el console.log de la funcion `,multiplique)
+    return multiplique
+}
+let resultadomulti = multiplique(a,b)
+console.log(`el resultado de la funcion es `,multiplique)
+
+//2 declaracion de parametros
+
+    /* let nombre=prompt(`ingrese nombre`)
+    mostrarnombre(nombre)
+    console.log(nombre) */
+
+//3 retorno de un valor
