fd26b801ec378b8794e907105affbb1f7b13b403
mrosales78 <mrosales78@gminit>
2023-01-16 21:20:09 -0300

    clase 15 commit

---
+++ clase5/app.js
@@ -0,0 +1,19 @@
+//array
+//metodo utilizado para reccorer un array es el for
+
+
+let array=[]
+let arraydos=[56,`hola mundo`,3.06,`t`,true,[`hola`,100]]
+
+//recorrer array
+for(let i=0;i<=5;i=i+1){
+console.log(`recorrido con un for`,arraydos[i])
+}
+
+let x=0
+
+while(x<=5){
+    console.log(`recorrido con un while`,arraydos[x])
+    x=x+1
+
+}
